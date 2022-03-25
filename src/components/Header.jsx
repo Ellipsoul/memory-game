fc2ae0ba02269018771d67239844554dca28a895
Aron Teh <aronteh.chess@gmail.com>
2022-03-25 21:26:29 +0000

    Updated NextJS picture, corrected grammar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,12 +2,16 @@ import React, { useEffect } from "react";
 
 const Header = ({ handleNewGame, wins }) => {
   // Update document title with number of wins
-  useEffect(() => (document.title = `Memory Game - ${wins} wins`));
+  useEffect(
+    () =>
+      (document.title = `Memory Game - ${wins} ${wins === 1 ? "Win" : "Wins"}`),
+    [wins]
+  );
 
   // Allow user to reset the game
   return (
     <header className="header">
-      <h4>{wins} Wins!</h4>
+      <h4>{`${wins} ${wins === 1 ? "Win" : "Wins"}!`}</h4>
       <h3>Memory Game</h3>
       <button onClick={handleNewGame}>Reset Game</button>
     </header>
